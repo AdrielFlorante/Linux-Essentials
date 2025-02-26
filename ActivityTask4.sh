fa179616ded164c5712a1a983f463d0928da77d7
Adriel Cedrick B. Florante <152822897+AdrielFlorante@users.noreply.github.com>
2025-02-26 00:46:13 +0000

    Modified ActivityTask4

--- ActivityTask4.sh
+++ ActivityTask4.sh
@@ -5,13 +5,13 @@ read -p 'Enter score: ' score
 
 if [[ $score -ge 90 && $score -le 100 ]]; then
     echo Grade A
-elif [[ $score -ge 80 ]]; then
+elif [[ $score -ge 80 && $score -le 100 ]]; then
     echo Grade B
-elif [[ $score -ge 70 ]]; then
+elif [[ $score -ge 70 && $score -le 100 ]]; then
     echo Grade C
-elif [[ $score -ge 60 ]]; then
+elif [[ $score -ge 60 && $score -le 100 ]]; then
     echo Grade D
-elif [[ $score -lt 60 ]]; then
+elif [[ $score -lt 60 && $score -ge 0 ]]; then
     echo Grade F
 else
     echo Error! Invalid input.
